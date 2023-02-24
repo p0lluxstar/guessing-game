c5b2f59f45f4737e1ec2478c4ffd235c721da657
global-p0lluxstar <global-web-registra@yandex.ru>
2023-02-24 22:06:05 +0300

    240223-22:05

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+       
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = Math.ceil((this.min + this.max) / 2);
+        return this.guessNum
     }
 
     lower() {
-
+        this.max = this.guessNum;
     }
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
